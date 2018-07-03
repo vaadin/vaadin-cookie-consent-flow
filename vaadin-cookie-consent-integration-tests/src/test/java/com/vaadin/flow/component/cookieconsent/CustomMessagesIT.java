8f491faeed9ae562356f4b34e521c373d6edc7ca
Tulio Garcia <tulio@vaadin.com>
2018-07-03 13:33:38 +0300

    Improved Testbench api

--- vaadin-cookie-consent-integration-tests/src/test/java/com/vaadin/flow/component/cookieconsent/CustomMessagesIT.java
+++ vaadin-cookie-consent-integration-tests/src/test/java/com/vaadin/flow/component/cookieconsent/CustomMessagesIT.java
@@ -19,15 +19,18 @@ public class CustomMessagesIT extends AbstractParallelTest {
         final CookieConsentElement element = $(CookieConsentElement.class)
                 .get(0);
         assertNotNull(element);
-        WebElement container = getDriver()
-                .findElement(By.className("cc-window"));
-        final WebElement dismiss = container
-                .findElement(By.className("cc-dismiss"));
-        assertNotNull(dismiss);
-        assertEquals("Cool!", dismiss.getText());
+        assertEquals(CustomMessages.POSITION, element.getPosition());
+        assertEquals(CustomMessages.MESSAGE, element.getMessage());
+        assertEquals(CustomMessages.DISMISS_LABEL, element.getDismissLabel());
+        assertEquals(CustomMessages.LEARN_MORE_LABEL,
+                element.getLearnMoreLabel());
+        assertEquals(CustomMessages.LEARN_MORE_LINK,
+                element.getLearnMoreLink());
+        final WebElement dismiss = element.getDismissLinkElement();
         dismiss.click();
         Thread.sleep(1000);
-        container = getDriver().findElement(By.className("cc-window"));
+        WebElement container = getDriver()
+                .findElement(By.className("cc-window"));
         assertFalse(container.isDisplayed());
     }
 
--- vaadin-cookie-consent-testbench/src/main/java/com/vaadin/flow/component/cookieconsent/testbench/CookieConsentElement.java
+++ vaadin-cookie-consent-testbench/src/main/java/com/vaadin/flow/component/cookieconsent/testbench/CookieConsentElement.java
@@ -1,9 +1,65 @@
 package com.vaadin.flow.component.cookieconsent.testbench;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.cookieconsent.CookieConsent.Position;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
 @Element("vaadin-cookie-consent")
 public class CookieConsentElement extends TestBenchElement {
+
+    public WebElement getDismissLinkElement() {
+        return getElementFromContainer(By.className("cc-dismiss"));
+    }
+
+    public WebElement getLearnMoreLinkElement() {
+        return getElementFromContainer(By.className("cc-link"));
+    }
+
+    public String getMessage() {
+        return getAttribute("message");
+    }
+
+    public String getDismissLabel() {
+        return getAttribute("dismiss");
+    }
+
+    public String getLearnMoreLabel() {
+        return getAttribute("learnMore");
+    }
+
+    public String getLearnMoreLink() {
+        return getAttribute("learnMoreLink");
+    }
+
+    public String getCookieName() {
+        return getAttribute("cookieName");
+    }
+
+    public Position getPosition() {
+        final String attributeValue = getAttribute("position");
+        final boolean hasValue = !Strings.isNullOrEmpty(attributeValue);
+        return hasValue ? Position.valueOf(attributeValue.replace('-', '_').toUpperCase())
+                : null;
+    }
+
+    public WebElement getContainer() {
+        return getDriver().findElement(By.className("cc-window"));
+    }
+ 
+    @Override
+    public SearchContext getContext() {
+        return getContainer();
+    }
+
+    WebElement getElementFromContainer(By by) {
+        final WebElement container = getContainer();
+        return container != null ? container.findElement(by) : null;
+
+    }
 
 }
